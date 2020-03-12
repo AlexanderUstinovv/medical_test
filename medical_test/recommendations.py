30365b827d25727bbb27083a6a29370e3a47701f
Alexander Ustinov <alexander.ustinov@protonmail.com>
2020-03-12 16:00:44 +0300

    Fixed bug with age fetching in recommendation logic

--- medical_test/recommendations.py
+++ medical_test/recommendations.py
@@ -3,8 +3,22 @@ from .models import MedicalProcedure, Age
 MALE = 'M'
 
 
+def fetch_age(age_value: int):
+    current_age = Age.objects.filter(value=age_value)
+    age = Age.objects.all().first()
+    if current_age.exists():
+        age = current_age.first()
+    else:
+        ages = Age.objects.all()
+        for item in ages:
+            if item.value > age_value:
+                age = item
+                break
+    return age
+
+
 def recommend_medical_test(sex: str, age: int):
-    age = Age.objects.get(value=age)
+    age = fetch_age(age)
 
     if sex == MALE:
         return MedicalProcedure.objects.filter(male=True,
