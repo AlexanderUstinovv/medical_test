8b90c962c14a410822f966f362b42b3d4d8f867f
Alexander Ustinov <alexander.ustinov@protonmail.com>
2020-03-12 12:01:06 +0300

    Added reference to app config

--- medical_test/apps.py
+++ medical_test/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class YourAppConfig(AppConfig):
+class MedicalTestAppConfig(AppConfig):
     name = 'medical_test'
     verbose_name = 'Мед. рекомендации по прохождению исследований'
